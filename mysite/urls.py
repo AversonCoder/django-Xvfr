dbfcfe406f78b9da10660f6e966840416fab9054
averson <averson.work@gmail.com>
2025-10-25 22:04:23 +0800

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('twitter/', include('twitter_monitor.urls')),
+    path('', RedirectView.as_view(url='/twitter/', permanent=False)),  # 首页跳转
 ]
--- twitter_monitor/urls.py
+++ twitter_monitor/urls.py
@@ -5,6 +5,9 @@ Twitter Monitor URL 配置
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from . import web_views
+
+app_name = 'twitter_monitor'
 
 router = DefaultRouter()
 router.register(r'monitored-users', views.MonitoredUserViewSet, basename='monitoreduser')
@@ -13,5 +16,14 @@ router.register(r'replies', views.ReplyViewSet, basename='reply')
 router.register(r'logs', views.MonitorLogViewSet, basename='monitorlog')
 
 urlpatterns = [
+    # Web 可视化界面
+    path('', web_views.dashboard, name='dashboard'),
+    path('add-user/', web_views.add_user, name='add_user'),
+    path('monitor-config/', web_views.monitor_config, name='monitor_config'),
+    path('start-monitoring/', web_views.start_monitoring, name='start_monitoring'),
+    path('user/<int:user_id>/', web_views.user_detail, name='user_detail'),
+    path('api-docs/', web_views.api_docs, name='api_docs'),
+    
+    # REST API
     path('api/', include(router.urls)),
 ]
